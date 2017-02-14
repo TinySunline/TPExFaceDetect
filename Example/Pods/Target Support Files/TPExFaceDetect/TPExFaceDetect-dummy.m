f7437b0ff644a8f9d982193bc4d28a980053326a
kingdomrain <bygd2014@sina.com>
2017-02-14 10:19:02 +0800

    移除工程
    移除工程

--- Example/Pods/Target Support Files/TPExFaceDetect/TPExFaceDetect-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TPExFaceDetect : NSObject
-@end
-@implementation PodsDummy_TPExFaceDetect
-@end
--- Example/Pods/Target Support Files/TPExFaceDetect/TPExFaceDetect-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double TPExFaceDetectVersionNumber;
-FOUNDATION_EXPORT const unsigned char TPExFaceDetectVersionString[];
-
