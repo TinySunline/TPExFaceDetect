9a356157ad0e8154bbb8b655d9be26242e71a741
kingdomrain <bygd2014@sina.com>
2017-02-23 11:52:06 +0800

    1.0.1   客户端将人脸识别的图片地址抛出，由服务端发起图片的对比
    1.0.1   客户端将人脸识别的图片地址抛出，由服务端发起图片的对比

--- TPExFaceDetect/TPExFaceDetect.mm
+++ TPExFaceDetect/TPExFaceDetect.mm
@@ -58,124 +58,134 @@
     
     [manager getLiveDetectCompletion:^(BOOL sueccess, NSData *imageData) {
         [manager dismissTakeCaptureSessionViewController];
-        //人脸识别获得图片资源
-        UIImage *image = [UIImage imageWithData:imageData];
-        //图片对比  底层完成图片对比
-        //NSData *imageDatabb = UIImageJPEGRepresentation(image, 1.0);
-        //        然后应用Base64编码将其转换成base64编码的字符串：
-        //NSString *encodedString1 = [imageData base64Encoding];
-        
-        NSString *encodedString1 = [imageData base64EncodedStringWithOptions:0];
-        
-        //身份证获得的图片资源    识别获得图片资源
-        // 本地沙盒目录
-        NSString *pathaa = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
-        // 得到本地沙盒中名为"MyImage"的路径，"MyImage"是保存的图片名
-        NSString *imageFilePathaa = [pathaa stringByAppendingPathComponent:@"/faceImage.jpg"];
-        UIImage *imageaa = [UIImage imageWithContentsOfFile:imageFilePathaa];
-        NSLog(@"image2 is size %@",NSStringFromCGSize(imageaa.size));
-        //        NSData *dataaa = [NSData dataWithContentsOfURL:[NSURL  URLWithString:imageFilePathaa]];
-        //        UIImage *imageaa = [UIImage imageWithData:dataaa]; // 取得图片
-        NSData *imageaaData = UIImageJPEGRepresentation(imageaa, 0.2);
-        NSString *encodedString2 = [imageaaData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];
-        
-       
-        //发起网络请求
-        YZFAFHTTPSessionManager *manager = [YZFAFHTTPSessionManager manager];
-        manager.requestSerializer = [AFHTTPRequestSerializer  serializer];
-        manager.responseSerializer = [AFHTTPResponseSerializer serializer];
-        [manager.responseSerializer setAcceptableContentTypes:[NSSet setWithObjects:@"application/json",@"application/xml",@"text/json",@"text/javascript",@"text/xml",@"text/plain",  nil]];
-        [manager.requestSerializer setTimeoutInterval:20];
-        
-        // 拼接请求参数
-        NSMutableDictionary *params = [NSMutableDictionary dictionary];
-        params[@"username"] = @"test";
-        params[@"password"] = @"test";
-        params[@"encoding"] = @"utf-8";
-        [manager POST:@"http://cloud.exocr.com/face" parameters:params constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
-            [formData appendPartWithFileData:imageData name:@"face1" fileName:@"face1.jpg" mimeType:@"image/jpg"];
-            [formData appendPartWithFileData:imageaaData name:@"face2" fileName:@"face2.jpg" mimeType:@"image/jpg"];
-            
-        } progress:^(NSProgress * _Nonnull uploadProgress) {
-            ;
-        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-            NSLog(@"request url =%@\n request dic = %@",task.response.URL ,params);
-            NSLog(@"response = \n%@",responseObject);
-            NSDictionary *json =[NSJSONSerialization JSONObjectWithData:responseObject options:kNilOptions
-                                                                  error:nil];
-            NSLog(@"json==%@",json);
-            if([json[@"Error"] isEqualToString:@"0"]){
-                
-                  NSString *score = json[@"Score"];
-                  NSString *th_high = json[@"th_high"];
-                
-                float scoreFloat =[score floatValue];
-                float th_highFloat =[th_high floatValue];
-                if (scoreFloat>=th_highFloat) {
-                    //发送公安的API请求
-                    YZFAFHTTPSessionManager *manager1 = [YZFAFHTTPSessionManager manager];
-                    manager1.requestSerializer = [AFHTTPRequestSerializer  serializer];
-                    manager1.responseSerializer = [AFHTTPResponseSerializer serializer];
-                    [manager1.responseSerializer setAcceptableContentTypes:[NSSet setWithObjects:@"application/json",@"application/xml",@"text/json",@"text/javascript",@"text/xml",@"text/plain",@"text/html",nil]];
-                    [manager1.requestSerializer setTimeoutInterval:20];
-                    NSMutableDictionary *aparams = [NSMutableDictionary dictionary];
-                    aparams[@"appId"] = @"test";           //应用ID
-                    aparams[@"appKey"] = @"test";          //应用Key
-                    aparams[@"personalName"] = self.personalName;   //个人姓名
-                    aparams[@"identityCardNo"] = self.identityCardNo; //身份证号
-                    [manager1 POST:@"http://id.exocr.com/verify" parameters:aparams constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
-                        [formData appendPartWithFileData:imageData name:@"collectPhoto" fileName:@"collectPhoto" mimeType:@"image/jpg"];
-                    } progress:^(NSProgress * _Nonnull uploadProgress) {
-                        ;
-                    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//        //人脸识别获得图片资源
+//        UIImage *image = [UIImage imageWithData:imageData];
+//        //图片对比  底层完成图片对比
+//        //NSData *imageDatabb = UIImageJPEGRepresentation(image, 1.0);
+//        //        然后应用Base64编码将其转换成base64编码的字符串：
+//        //NSString *encodedString1 = [imageData base64Encoding];
+//        
+//        NSString *encodedString1 = [imageData base64EncodedStringWithOptions:0];
+//        
+//        //身份证获得的图片资源    识别获得图片资源
+//        // 本地沙盒目录
+//        NSString *pathaa = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+//        // 得到本地沙盒中名为"MyImage"的路径，"MyImage"是保存的图片名
+//        NSString *imageFilePathaa = [pathaa stringByAppendingPathComponent:@"/faceImage.jpg"];
+//        UIImage *imageaa = [UIImage imageWithContentsOfFile:imageFilePathaa];
+//        NSLog(@"image2 is size %@",NSStringFromCGSize(imageaa.size));
+//        //        NSData *dataaa = [NSData dataWithContentsOfURL:[NSURL  URLWithString:imageFilePathaa]];
+//        //        UIImage *imageaa = [UIImage imageWithData:dataaa]; // 取得图片
+//        NSData *imageaaData = UIImageJPEGRepresentation(imageaa, 0.2);
+//        NSString *encodedString2 = [imageaaData base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+//        
+//       
+//        //发起网络请求
+//        YZFAFHTTPSessionManager *manager = [YZFAFHTTPSessionManager manager];
+//        manager.requestSerializer = [AFHTTPRequestSerializer  serializer];
+//        manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+//        [manager.responseSerializer setAcceptableContentTypes:[NSSet setWithObjects:@"application/json",@"application/xml",@"text/json",@"text/javascript",@"text/xml",@"text/plain",  nil]];
+//        [manager.requestSerializer setTimeoutInterval:20];
+//        
+//        // 拼接请求参数
+//        NSMutableDictionary *params = [NSMutableDictionary dictionary];
+//        params[@"username"] = @"test";
+//        params[@"password"] = @"test";
+//        params[@"encoding"] = @"utf-8";
+//        [manager POST:@"http://cloud.exocr.com/face" parameters:params constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+//            [formData appendPartWithFileData:imageData name:@"face1" fileName:@"face1.jpg" mimeType:@"image/jpg"];
+//            [formData appendPartWithFileData:imageaaData name:@"face2" fileName:@"face2.jpg" mimeType:@"image/jpg"];
+//            
+//        } progress:^(NSProgress * _Nonnull uploadProgress) {
+//            ;
+//        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//            NSLog(@"request url =%@\n request dic = %@",task.response.URL ,params);
+//            NSLog(@"response = \n%@",responseObject);
+//            NSDictionary *json =[NSJSONSerialization JSONObjectWithData:responseObject options:kNilOptions
+//                                                                  error:nil];
+//            NSLog(@"json==%@",json);
+//            if([json[@"Error"] isEqualToString:@"0"]){
+//                
+//                  NSString *score = json[@"Score"];
+//                  NSString *th_high = json[@"th_high"];
+//                
+//                float scoreFloat =[score floatValue];
+//                float th_highFloat =[th_high floatValue];
+//                if (scoreFloat>=th_highFloat) {
+//                    //发送公安的API请求
+//                    YZFAFHTTPSessionManager *manager1 = [YZFAFHTTPSessionManager manager];
+//                    manager1.requestSerializer = [AFHTTPRequestSerializer  serializer];
+//                    manager1.responseSerializer = [AFHTTPResponseSerializer serializer];
+//                    [manager1.responseSerializer setAcceptableContentTypes:[NSSet setWithObjects:@"application/json",@"application/xml",@"text/json",@"text/javascript",@"text/xml",@"text/plain",@"text/html",nil]];
+//                    [manager1.requestSerializer setTimeoutInterval:20];
+//                    NSMutableDictionary *aparams = [NSMutableDictionary dictionary];
+//                    aparams[@"appId"] = @"96973102562410501";           //应用ID   96973102562410501
+//                    aparams[@"appKey"] = @"ee6f9e25690e4e639d2bbb2f0715f6ee";          //应用Key  ee6f9e25690e4e639d2bbb2f0715f6ee
+//                    aparams[@"personalName"] = self.personalName;   //个人姓名
+//                    aparams[@"identityCardNo"] = self.identityCardNo; //身份证号
+//                    //                aparams[@"encoding"] = @"utf-8";
+//                    [manager1 POST:@"http://id.exocr.com/verify" parameters:aparams constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+//                        [formData appendPartWithFileData:imageData name:@"collectPhoto" fileName:@"collectPhoto" mimeType:@"image/jpg"];
+//                    } progress:^(NSProgress * _Nonnull uploadProgress) {
+//                        ;
+//                    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+////                        NSLog(@"request url =%@\n request dic = %@",task.response.URL ,aparams);
+////                        NSLog(@"response111 = \n%@",responseObject);
+//                        NSDictionary *json =[NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableContainers
+//                                                                              error:nil];
+//                        
+//                        NSLog(@"json111===%@",json);
+//                        NSString *code = json[@"code"];
+//                        if([code isEqualToString:@"1001"]){
+//                            //                        NSString *desc = json[@"entity"][@"desc"];
+//                            NSString *score = json[@"entity"][@"score"];
+//                            int intScore =[score intValue];
+//                            if (intScore>40) {
+//                                NSString *backData = [NSString stringWithFormat:@"{\"code\":\"%@\",\"desc\":\"%@\"}",@"0",@"系统判断为同一人"];
+//                                [self.success callWithArguments:@[backData]];
+//                                
+//                            }
+//                            else if(intScore>30&&intScore<=40){
+//                                NSString *backData = [NSString stringWithFormat:@"{\"code\":\"%@\",\"desc\":\"%@\"}",@"2",@"不能确定是否为同人"];
+//                                [self.success callWithArguments:@[backData]];
+//                                
+//                            }
+//                            else if(intScore<=30)
+//                            {
+//                                NSString *backData = [NSString stringWithFormat:@"{\"code\":\"%@\",\"desc\":\"%@\"}",@"1",@"系统判断为不同人"];
+//                                [self.success callWithArguments:@[backData]];
+//                            }
+//                        }
+//                        else{
+//                            NSString *backData = [NSString stringWithFormat:@"{\"code\":\"%@\",\"desc\":\"%@\"}",@"2",@"不能确定是否是同一个人"];
+//                            [self.success callWithArguments:@[backData]];
+//                        }
+//                        
+//                    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
 //                        NSLog(@"request url =%@\n request dic = %@",task.response.URL ,aparams);
-//                        NSLog(@"response111 = \n%@",responseObject);
-                        NSDictionary *json =[NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableContainers
-                                                                              error:nil];
-                        
-                        NSLog(@"json111===%@",json);
-                        NSNumber *code = json[@"code"];
-                        int codeInt = [code intValue];
-                        if(codeInt == 1001){
-                            //                        NSString *desc = json[@"entity"][@"desc"];
-                            NSString *score = json[@"entity"][@"score"];
-                            int intScore =[score intValue];
-                            if (intScore>40) {
-                                NSString *backData = [NSString stringWithFormat:@"{\"code\":\"%@\",\"desc\":\"%@\"}",@"0",@"系统判断为同一人"];
-                                [self.success callWithArguments:@[backData]];
-                                
-                            }
-                            else if(intScore>30&&intScore<=40){
-                                NSString *backData = [NSString stringWithFormat:@"{\"code\":\"%@\",\"desc\":\"%@\"}",@"2",@"不能确定是否为同人"];
-                                [self.success callWithArguments:@[backData]];
-                                
-                            }
-                            else if(intScore<=30)
-                            {
-                                NSString *backData = [NSString stringWithFormat:@"{\"code\":\"%@\",\"desc\":\"%@\"}",@"1",@"系统判断为不同人"];
-                                [self.success callWithArguments:@[backData]];
-                            }
-                        }
-                        else{
-                            NSString *backData = [NSString stringWithFormat:@"{\"code\":\"%@\",\"desc\":\"%@\"}",@"2",@"不能确定是否是同一个人"];
-                            [self.success callWithArguments:@[backData]];
-                        }
-                        
-                    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-                        NSLog(@"request url =%@\n request dic = %@",task.response.URL ,aparams);
-                        NSLog(@"error = \n%@",error.localizedDescription);
-                    }];
-   
-                }
-            }
-            else{
-                NSString *backData = [NSString stringWithFormat:@"{\"code\":\"%@\",\"desc\":\"%@\"}",@"2",@"不能确定是否是同一个人"];
-                [self.success callWithArguments:@[backData]];
-            }
-        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-            NSLog(@"request url =%@\n request dic = %@",task.response.URL ,params);
-            NSLog(@"error = \n%@",error.localizedDescription);
-        }];
+//                        NSLog(@"error = \n%@",error.localizedDescription);
+//                    }];
+//   
+//                }
+//            }
+//            else{
+//                NSString *backData = [NSString stringWithFormat:@"{\"code\":\"%@\",\"desc\":\"%@\"}",@"2",@"不能确定是否是同一个人"];
+//                [self.success callWithArguments:@[backData]];
+//            }
+//        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//            NSLog(@"request url =%@\n request dic = %@",task.response.URL ,params);
+//            NSLog(@"error = \n%@",error.localizedDescription);
+//        }];
+        
+        //方案二: 客户端将人脸识别的图片地址抛出，由服务端发起图片的对比
+        NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+        UIImage *exFaceImg =[UIImage imageWithData:imageData];    //身份证背面全图
+        NSString *exFaceImgPath = [path stringByAppendingPathComponent:@"/exFaceImg.jpg"];
+        BOOL success = [UIImageJPEGRepresentation(exFaceImg, 0.5) writeToFile:exFaceImgPath  atomically:YES];
+        if(success){
+            NSString *bdata = [NSString stringWithFormat:@"{\"mMove\":\"%@\",\"mRezion\":\"%@\",\"imgPath\":\"%@\",\"isLivePassed\":\"%@\"}",@"",@"",exFaceImgPath,@"true"];
+            [self.success callWithArguments:@[bdata]];
+        }
     }
       cancel:^(BOOL sueccess, NSString *error) {
           [self.error callWithArguments:@[@"取消"]];
